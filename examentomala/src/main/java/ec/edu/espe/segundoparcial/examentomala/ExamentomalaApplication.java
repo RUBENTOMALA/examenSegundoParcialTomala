b535676e02c8b7db2f905cab32a334909890f4cb
RUBEN ALFREDO TOMALA VERA <ruben.al97@hotmail.com>
2024-02-05 19:01:23 -0500

    Inicial Commit

---
+++ examentomala/src/main/java/ec/edu/espe/segundoparcial/examentomala/ExamentomalaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examentomala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamentomalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamentomalaApplication.class, args);
+	}
+
+}
---
+++ examentomala/src/test/java/ec/edu/espe/segundoparcial/examentomala/ExamentomalaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examentomala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamentomalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
